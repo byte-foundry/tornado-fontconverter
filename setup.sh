5b29cda55909c7dd9782e210ad418e1f7b23516a
Bill <bill.rousseau@student.42.fr>
2014-08-08 15:51:14 +0200

    setup script updated with pm2 configuration

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> .bashrc
 apt-get -y update
+curl -L https://npmjs.org/install.sh | sh
+npm install pm2@latest -g
 apt-get -y install packaging-dev pkg-config python-dev libpango1.0-dev libglib2.0-dev libxml2-dev giflib-dbg libjpeg-dev libtiff-dev uthash-dev
 apt-get -y install git
 git clone https://github.com/fontforge/fontforge.git ff
@@ -15,3 +17,4 @@ cd tornado-4.0
 python setup.py build
 python setup.py install
 cd ..
+pm2 start py-fontconv.py
