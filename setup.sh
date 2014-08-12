6851235447a553040a5b9e38e6419bfa95f0b7db
Bill <bill.rousseau@student.42.fr>
2014-08-12 13:59:55 +0200

    setup script updated: it works

--- setup.sh
+++ setup.sh
@@ -1,20 +1,39 @@
+echo "#####     CREATING .BASHRC FILE AND EDITING IT WITH LIBRAIRIES     #####"
+touch .bashrc && chmod 777 .bashrc
 echo "export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH" >> .bashrc
+source .bashrc && echo "source bashrc complete"
+echo "#####     UPDATING APT-GET     #####"
 apt-get -y update
-curl -L https://npmjs.org/install.sh | sh
-npm install pm2@latest -g
-apt-get -y install packaging-dev pkg-config python-dev libpango1.0-dev libglib2.0-dev libxml2-dev giflib-dbg libjpeg-dev libtiff-dev uthash-dev
+echo "#####     INSTALLING GIT...    #####"
 apt-get -y install git
+echo "#####     INSTALLING NODEJS...     #####"
+apt-get -y install nodejs
+ln -s /usr/bin/nodejs /usr/bin/node
+echo "#####     INSTALLING MAKE...     #####"
+apt-get -y install make
+echo "#####     INSTALLING NPM FROM GITHUB...     #####"
+git clone https://github.com/npm/npm.git
+cd npm
+sudo make
+sudo make install
+cd ..
+echo "#####     INSTALLING PM2...    #####"
+sudo npm install pm2@latest -g
+echo "#####     UPDATING APT-GET     #####"
+apt-get -y update
+echo "#####     INSTALLING DEPENDENCIES FOR FONTFORGE...    #####"
+apt-get -y install packaging-dev pkg-config python-dev libpango1.0-dev libglib2.0-dev libxml2-dev giflib-dbg libjpeg-dev libtiff-dev uthash-dev
+echo "#####     INSTALLING FONTFORGE FROM SOURCES...    #####"
 git clone https://github.com/fontforge/fontforge.git ff
 cd ff
 ./bootstrap
 ./configure
-make
+sudo make
 sudo make install
 cd ..
-wget https://pypi.python.org/packages/source/t/tornado/tornado-4.0.tar.gz
-tar xvzf tornado-4.0.tar.gz
-cd tornado-4.0
-python setup.py build
-python setup.py install
-cd ..
-pm2 start py-fontconv.py
+echo "#####     INSTALLING TORNADO FROM PIP...    #####"
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install tornado
+echo "#####     LAUNCHING PM2 START...    #####"
+pm2 start /home/bill/py-fontconv.py -x --interpreter python
