a40d31c3db367a4828ef15ce240cebbff59542e7
Bill <vagrant@precise32.(none)>
2014-09-03 15:02:41 +0000

    deletion command

---
+++ delete.py
@@ -0,0 +1,14 @@
+import tornado, tornado.web
+import boto, boto.s3
+import os
+from boto.s3.connection import S3Connection
+from bucket import Bucket
+
+class Delete(tornado.web.RequestHandler):
+	def post(self):
+		deletedpath = self.get_argument("deletedpath")
+		conn = S3Connection(os.environ['ACCESS_KEY'], os.environ['SECRET_KEY'])
+		bucket = conn.get_bucket('allfonts')
+		#try:
+		bucketListResultSet = bucket.list(prefix=deletedpath)
+		result = bucket.delete_keys([key.name for key in bucketListResultSet])
